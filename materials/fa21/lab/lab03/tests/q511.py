48290ada19cc36de61bc6c67922133b0cd3eeefa
Stephanie Djajadi <sdjajadi@berkeley.edu>
2021-09-06 21:03:28 -0700

    release lab03

---
+++ materials/fa21/lab/lab03/tests/q511.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q511',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> type(collection_times) == np.ndarray\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(collection_times)\n744', 'hidden': False, 'locked': False},
+                                   {'code': '>>> all(collection_times == np.arange(0, 31*24*60*60, 60*60))\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
