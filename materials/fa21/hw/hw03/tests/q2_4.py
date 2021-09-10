8954dcb498558d30801cab95ffe55eaf33686d1f
Melissa Wong <melissarwong@berkeley.edu>
2021-09-10 10:56:23 -0700

    release hw03

---
+++ materials/fa21/hw/hw03/tests/q2_4.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_4',
+    'points': [0, 0],
+    'suites': [   {   'cases': [{'code': '>>> type(visualization) == int\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> 1 <= visualization <= 3\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
