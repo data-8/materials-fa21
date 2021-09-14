c7b756b67b5cae1cab3a4e4ef867626d3894f6a0
Stephanie Djajadi <sdjajadi@berkeley.edu>
2021-09-13 21:09:20 -0700

    release lab04

---
+++ materials/fa21/lab/lab04/tests/q12.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q12',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> a_percentage == 70.71067811865476\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
