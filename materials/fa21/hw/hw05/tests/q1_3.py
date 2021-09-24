5d04c6df83a68e824a33db16b89955cd09f1f802
Melissa Wong <melissarwong@berkeley.edu>
2021-09-24 10:34:41 -0700

    Add hw05

---
+++ materials/fa21/hw/hw05/tests/q1_3.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_3',
+    'points': [0, 20],
+    'suites': [   {   'cases': [   {'code': ">>> 'did_cal_lose' in globals()\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> did_cal_lose(final_scores.row(1))\nFalse', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ materials/fa21/hw/hw05/tests/q1_5.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_5',
+    'points': [5, 5],
+    'suites': [   {   'cases': [   {'code': '>>> is_big_win(final_scores.row(0)) is True\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(big_wins) == np.ndarray\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
