724d3ca9b4dcdcd32036018b65c2bd70043e657a
Stephanie Djajadi <sdjajadi@berkeley.edu>
2021-10-18 19:43:33 -0700

    release lab07

---
+++ materials/fa21/lab/lab07/tests/q3.py
@@ -0,0 +1,1 @@
+test = {'name': 'q3', 'points': None, 'suites': [{'cases': [{'code': '>>> submitted\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
