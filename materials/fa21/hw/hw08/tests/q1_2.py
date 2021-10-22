cdda5cc0b095c6261ecba9b8c3f31cc00c29fd8b
Melissa Wong <melissarwong@berkeley.edu>
2021-10-22 11:36:59 -0700

    Release hw08

---
+++ materials/fa21/hw/hw08/tests/q1_2.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_2',
+    'points': [0],
+    'suites': [   {   'cases': [{'code': ">>> # Adding default public test so Otter doesn't error.\n>>> True == True\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
