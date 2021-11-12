bb2c383ed26826eaed178b66974f1ddda5f6d923
Melissa Wong <melissarwong@berkeley.edu>
2021-11-12 11:50:41 -0800

    Release hw11

---
+++ materials/fa21/hw/hw11/tests/q1_3.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_3',
+    'points': [0],
+    'suites': [   {   'cases': [{'code': '>>> len(fit_line(Table().with_columns(\'x\', make_array(0, 1), \'y\', make_array(1, 3)), "x", "y")) == 2\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
