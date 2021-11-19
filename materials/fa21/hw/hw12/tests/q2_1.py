f971b3beaeb5cc65444f8cfcc0dce64e77682142
Melissa Wong <melissarwong@berkeley.edu>
2021-11-19 11:54:23 -0800

    Release hw12

---
+++ materials/fa21/hw/hw12/tests/q2_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_1', 'points': [0], 'suites': [{'cases': [{'code': '>>> len(secret_word) > 0\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
